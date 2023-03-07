67c83ec9cf7cab6abb02653915aeb99949171d66
abdelilah <marrouss.abdelilah@gmail.com>
2023-03-07 18:59:40 +0100

    first commit, crud works good

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import {UserModule} from "./user/user.module";
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/userDB'), UserModule,],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/userDB'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
